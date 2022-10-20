f64b30a6b41070baa08251a437e73c3abd532159
neegam1512 <neegamananda2000@gmail.com>
2022-10-20 21:53:58 +0530

    Second Update

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ import videoRoutes from "./routes/videos.js";
 import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
-
+import cors from 'cors';
 
 const app = express();
 dotenv.config();
+
+
+
+app.use(cors())
+
+
 
 const connect = () => {
   mongoose
@@ -21,6 +27,13 @@ const connect = () => {
       throw err;
     });
 };
+
+app.get('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 //middlewares
 app.use(cookieParser())
--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ router.delete("/:id", verifyToken, deleteUser);
 router.get("/find/:id", getUser);
 
 //subscribe a user
-router.put("/sub/:id", verifyToken, subscribe);
+router.put("/sub/:id",  subscribe);
 
 //unsubscribe a user
 router.put("/unsub/:id", verifyToken, unsubscribe);
